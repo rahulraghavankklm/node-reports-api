e8c7708fd6eac60e6fc3d59a4c0c851455be300f
Rahul Raghavan <rahulraghavankklm@gmail.com>
2018-05-23 17:17:12 +0530

    configured new db

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/reports-node');
+mongoose.connect('mongodb://127.0.0.1:27017/reports-node-new');
 
 const db = mongoose.connection
 
